1a25351247dbe9a868a97a1b959d91e281670e8d
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-09 00:53:41 +0530

    Add: Inserted in Middle

--- src/test/java/com/capgemini/linkedlist/MyNodeTest.java
+++ src/test/java/com/capgemini/linkedlist/MyNodeTest.java
@@ -7,13 +7,12 @@ public class MyNodeTest {
 
 	@Test
 	public void given3NumbersAddedToList() {
-	MyNode<Integer> myFirstNode = new MyNode<>(56);
-	MyNode<Integer> mySecondNode = new MyNode<>(30);
-	MyNode<Integer> myThirdNode = new MyNode<>(70);
-	myFirstNode.setNext(mySecondNode);
-	mySecondNode.setNext(myThirdNode);
-	boolean result = myFirstNode.getNext().equals(mySecondNode)&&
-					 mySecondNode.getNext().equals(myThirdNode);
-	Assert.assertTrue(result);
-}
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
 }
