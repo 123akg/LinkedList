1f98a869f385fbe66d8d80ebc2cf0dfd587047d2
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-09 00:22:07 +0530

    Welcome to My First LinkedList Program

---
+++ src/main/java/com/capgemini/linkedlist/MyNode.java
@@ -0,0 +1,10 @@
+package com.capgemini.linkedlist;
+
+import java.util.*;
+
+public class MyNode{
+	
+	public static void main (String args[]) {
+	System.out.println("Welcome to My First LinkedList Program ");
+	}
+}
